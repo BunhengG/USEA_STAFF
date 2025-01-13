eef1a5002adf49888ec81c3d9de8a88998bdb1fc
BunhengG <bbmc255@gmail.com>
2025-01-13 14:09:02 +0700

    Updated UI

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,9 +1,15 @@
+import 'dart:ui';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:usea_staff_test/constant/constant.dart';
 import '../../auth/login_screen.dart';
 import '../../helper/shared_pref_helper.dart';
 import 'package:quickalert/quickalert.dart';
+
+import '../../provider/mycard_provider.dart';
+import 'widget/home_card.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -17,22 +23,14 @@ class ProfileScreen extends StatelessWidget {
       text: 'Are you sure you want to leave this company?',
       confirmBtnText: 'Sign Out',
       cancelBtnText: 'Cancel',
-      headerBackgroundColor: Theme.of(context).primaryColor,
+      headerBackgroundColor: primaryColor,
       backgroundColor: secondaryColor,
-      borderRadius: 26,
-      textColor: titleColor,
-      titleColor: Theme.of(context).primaryColor,
-      confirmBtnColor: Colors.redAccent,
-      cancelBtnTextStyle: const TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 16,
-        color: titleColor,
-      ),
-      confirmBtnTextStyle: const TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 16,
-        color: secondaryColor,
-      ),
+      borderRadius: roundedCornerSM,
+      textColor: textColor,
+      titleColor: uAtvColor,
+      confirmBtnColor: uAtvColor,
+      cancelBtnTextStyle: getWhiteSubTitle(),
+      confirmBtnTextStyle: getWhiteSubTitle(),
       onConfirmBtnTap: () async {
         Navigator.pop(context);
         await _performLogoutActions(context);
@@ -67,24 +65,246 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: Text(
+          'Profile',
+          style: getSubTitle(),
+        ),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: IconButton(
+            icon: Image.asset(
+              'assets/icon/back.png',
+              scale: 12,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () => _logout(context),
-            icon: const FaIcon(
-              FontAwesomeIcons.signOutAlt,
-              color: Colors.redAccent,
+            icon: Image.asset(
+              'assets/icon/signout.png',
+              scale: 8,
             ),
             tooltip: 'Sign Out',
           ),
         ],
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Consumer<CardProvider>(builder: (context, cardProvider, child) {
+          // Show loading indicator
+          if (cardProvider.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(color: primaryColor),
+            );
+          }
+
+          // Show error message if any
+          if (cardProvider.errorMessage != null) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  cardProvider.errorMessage!,
+                  style: getSubTitle().copyWith(color: uAtvColor),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+
+          // Show no records found if the list is empty
+          if (cardProvider.cards.isEmpty) {
+            return const Center(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'No Card records found.',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+
+          // Display the Card card at the top
+          return Column(
+            children: [
+              // Display the list of Cards
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: cardProvider.cards.length,
+                itemBuilder: (context, index) {
+                  final card = cardProvider.cards[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.25,
+                        width: double.infinity,
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Stack(
+                                children: [
+                                  CachedNetworkImage(
+                                    imageUrl: card.image,
+                                    fit: BoxFit.fitWidth,
+                                    width: double.infinity,
+                                    placeholder: (context, url) =>
+                                        const CircularProgressIndicator(),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                  ),
+                                  Positioned.fill(
+                                    child: BackdropFilter(
+                                      filter: ImageFilter.blur(
+                                        sigmaX: 16.0,
+                                        sigmaY: 16.0,
+                                      ),
+                                      child: Container(
+                                        width: double.infinity,
+                                        color: Colors.black45.withOpacity(0.1),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // CircleAvatar on top
+                            Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 46,
+                                    backgroundColor: secondaryColor,
+                                    child: CachedNetworkImage(
+                                      imageUrl: card.image,
+                                      placeholder: (context, url) =>
+                                          const CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) =>
+                                          const Icon(Icons.error),
+                                      fit: BoxFit.cover,
+                                      scale: 6,
+                                    ),
+                                  ),
+                                  _buildTextDetailsProfile('', card.name, 20),
+                                  _buildTextDetailsProfile(
+                                    'ID: ',
+                                    card.userId,
+                                    12,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: defaultMargin * 2),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 12.0),
+                            child: WorkingPeriodSection(),
+                          ),
+                          const SizedBox(height: defaultMargin),
+                          _buildTextDetails(
+                            FontAwesomeIcons.user,
+                            card.name,
+                          ),
+                          _buildTextDetails(
+                            FontAwesomeIcons.idCard,
+                            card.userId,
+                          ),
+                          _buildTextDetails(
+                            FontAwesomeIcons.transgenderAlt,
+                            card.gender == 'M' ? 'Male' : 'Female',
+                          ),
+                          _buildTextDetails(
+                            FontAwesomeIcons.calendarAlt,
+                            card.dob!.toLocal().toString().split(' ')[0],
+                          ),
+                          _buildTextDetails(
+                            FontAwesomeIcons.briefcase, // Icon for "Position"
+                            card.position,
+                          ),
+                        ],
+                      )
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: defaultPadding),
+              ),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+
+  _buildTextDetailsProfile(
+    String title,
+    String value,
+    double size,
+  ) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: getSubTitle().copyWith(color: secondaryColor, fontSize: size),
+        ),
+        Text(
+          value,
+          style: getSubTitle().copyWith(color: secondaryColor, fontSize: size),
+        ),
+      ],
+    );
+  }
+
+  _buildTextDetails(IconData icon, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: defaultPadding,
+        vertical: smPadding,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(defaultPadding),
+        decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.circular(roundedCornerSM),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text('Welcome to the Profile App!'),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: smPadding,
+                right: defaultPadding,
+              ),
+              child: FaIcon(
+                icon,
+                color: primaryColor,
+              ),
+            ),
+            Text(
+              value,
+              style: getSubTitle(),
+            ),
           ],
         ),
       ),
